af1522c4eb684519d180abe348d089acd55b85d8
akiko <akikomoore8@gmail.com>
2025-02-24 02:35:54 +0700

    using components

--- js/main.js
+++ js/main.js
@@ -1,33 +1,73 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        brand:'Vue Mastery',
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.',
-        inventory: 11,
-        onSale:false,
-        details:['80% cotton', '20% polyester', 'Gender-neutral'],
-        sizes:['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: './assets/vmSocks-green-onWhite.jpg',
-                variantQuantity: 10,
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: './assets/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 0,
-            }
-        ],
-        cart: 0,
+Vue.component('product', {
+    template:`<div class='product'>
+    <div class="product-image">
+            <img alt="#" v-bind:src="image" :alt="altText"/>
+        </div>
 
+        <div class="product-info">
+            <h1>{{ title }}</h1>
 
-},
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+
+            <ul>
+                <li v-for="size in sizes">{{size}}</li>
+            </ul>
+
+            <div
+                    class="color-box"
+                    v-for="(variant, index) in variants"
+                    :key="variant.variantId"
+                    :style="{ backgroundColor:variant.variantColor }"
+                    @mouseover="updateProduct(index)"
+            >
+            </div>
+
+            <p v-if="inventory > 10">In Stock</p>
+            <p v-else-if="inventory <=10 && inventory > 0">Almost sold out!</p>
+            <p v-else :class="{ outOfStock: !inStock}">Out of stock</p>
+
+            <span> {{sale}}</span>
+        </div>
+
+        <div class="cart">
+            <p>Cart({{ cart }})</p>
+        </div>
+
+        <button v-on:click="addToCart" :disabled="!inStock" :class="{disabledButton: !inStock}">Add to cart</button>
+        <button v-on:click="removeFromCart">Remove from cart</button>
+        
+        <a :href="link">More products like this</a>
+</div>`,
+    data(){
+        return{
+            product: "Socks",
+            brand:'Vue Mastery',
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.',
+            inventory: 11,
+            onSale:false,
+            details:['80% cotton', '20% polyester', 'Gender-neutral'],
+            sizes:['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: './assets/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10,
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0,
+                }
+            ],
+            cart: 0
+        }
+    },
     methods: {
         addToCart() {
             this.cart += 1
@@ -40,10 +80,8 @@ let app = new Vue({
 
         removeFromCart() {
             this.cart -= 1;
-        },
-
+        }
     },
-
     computed: {
         title() {
             return this.brand + ' ' + this.product;
@@ -66,15 +104,6 @@ let app = new Vue({
     }
 })
 
-Vue.component('product', {
-    template: `
-    <div class="product">
-    <div class="product-image">
-            <img alt="#" v-bind:src="image" :alt="altText"/>
-</div>`,
-    data(){
-        return{}
-    },
-    methods: {},
-    computed: {}
+let app = new Vue({
+    el: '#app',
 })
